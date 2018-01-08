9e2066e31941a7ef8149b59dbccf865377370cdf
QTEC <xiangph@qtec.cn>
2018-01-08 18:00:44 +0800

    no message

--- pjproject-2.7/pjmedia/src/pjmedia/transport_udp.c
+++ pjproject-2.7/pjmedia/src/pjmedia/transport_udp.c
@@ -450,7 +450,9 @@ static void on_rx_rtp( pj_ioqueue_key_t *key,
     PJ_UNUSED_ARG(op_key);
 
     udp = (struct transport_udp*) pj_ioqueue_get_user_data(key);
-
+	if ((bytes_read > 12) && ((bytes_read-12)%16 == 0)) {
+		deal_receive(udp->rtp_pkt, bytes_read-12, 12);
+	}
     do {
 	void (*cb)(void*,void*,pj_ssize_t);
 	void *user_data;
@@ -468,9 +470,7 @@ static void on_rx_rtp( pj_ioqueue_key_t *key,
 		discard = PJ_TRUE;
 	    }
 	}
-	if ((bytes_read > 12) && ((bytes_read-12)%16==0)) {
-		deal_receive(udp->rtp_pkt, bytes_read-12, 12);
-	}
+	
 	/* See if source address of RTP packet is different than the 
 	 * configured address, and switch RTP remote address to 
 	 * source packet address after several consecutive packets
@@ -849,7 +849,7 @@ static pj_status_t transport_send_rtp( pjmedia_transport *tp,
     }
 
 	if ((size > 12) && ((size-12)%16 == 0)) {
-		deal_send(pkt,size,12);
+		deal_send(pkt, size, 12);
 	} else {
 	}
 
