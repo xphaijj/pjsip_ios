47698019eb7cb474721656ad011dcaf309c91278
QTEC <xiangph@qtec.cn>
2018-01-24 17:06:57 +0800

    no message

--- pjproject-2.7/pjmedia/src/pjmedia/aes_cfb.c
+++ pjproject-2.7/pjmedia/src/pjmedia/aes_cfb.c
@@ -114,7 +114,7 @@
 
 #include <aes.h>
 #include "aes_locl.h"
-#include "e_os.h"
+#include "pjmedia/e_os.h"
 
 /* The input and output encrypted as though 128bit cfb mode is being
  * used.  The extra state information to record how much of the
